e050f4ef4ab5a36e084a99d8d4160227b89e9763
Yune Me Me Mon <39293160+YuneMeMeMon@users.noreply.github.com>
2018-06-08 14:25:16 +0000

    Add function

--- hello.py
+++ hello.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/Yune")
-def Yune():
+@app.route("/")
+def hello():
     return "Hello World"
+
+@app.route("/Yoon")
+def Yoon():
+    return "Hello Yoon Me Me Mon"
+    
